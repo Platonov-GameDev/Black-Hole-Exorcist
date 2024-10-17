1d1af06c6ac45d58659685dedab7f47ef941a676
Kirill Platonov <platonovk1998@ya.ru>
2024-10-17 21:08:50 +0500

    arrows stay impaled into enemy after hit

--- scenes/Bullet/Bullet.gd
+++ scenes/Bullet/Bullet.gd
@@ -2,9 +2,13 @@ extends CharacterBody2D
 
 
 @onready var visible_on_screen_notifier_2d = $VisibleOnScreenNotifier2D
+@onready var collision_polygon_2d = $CollisionPolygon2D
 
 var MOVE_SPEED = 600.0
-var damage = 35
+var DAMAGE = 35
+var IMPALE_DEPTH = 10
+
+var did_hit := false
 
 
 func _ready():
@@ -14,19 +18,19 @@ func _ready():
 
 
 func _physics_process(delta):
+	if did_hit: return
 	var time_coefficient = GameManager.calculate_time_coefficient(position)
 	var collision = move_and_collide(
 		Vector2.RIGHT.rotated(rotation) * MOVE_SPEED * delta * time_coefficient
 	)
 	if collision:
 		var collider = collision.get_collider() as Node
-		collider.health_component.take_damage(35)
-		expire()
+		collider.health_component.take_damage(DAMAGE)
+		collision_polygon_2d.disabled = true
+		position = collision.get_position() + Vector2.RIGHT.rotated(rotation) * IMPALE_DEPTH
+		reparent(collider)
+		did_hit = true
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	expire()
-
-
-func expire():
 	call_deferred("queue_free")
